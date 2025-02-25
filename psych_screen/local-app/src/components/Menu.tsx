c19631ac92d7fb2aaa3a23e233a4e854e8ebeefd
bgkresge <Ben.Kresge@UMassMed.edu>
2025-02-25 19:21:14 +0000

    Added all 30 samples from paper to dropdown selection.

--- psych_screen/local-app/src/components/Menu.tsx
+++ psych_screen/local-app/src/components/Menu.tsx
@@ -1,17 +1,43 @@
 import React, { useState } from "react";
 import DropDown from "./DropDown";
 
-const Menu: React.FC<{ sampleSelection: (sample: string) => void }> = ({sampleSelection}): JSX.Element => {
-    
+const Menu: React.FC<{ sampleSelection: (sample: string) => void }> = ({ sampleSelection }): JSX.Element => {
+
   const [showDropDown, setShowDropDown] = useState<boolean>(false);
   const [selectedSample, setSelectedSample] = useState<string>("DLPFC_Br8667_mid_manual_alignment_all");
 
   const samples = () => {
     return [
-        "DLPFC_Br8667_mid_manual_alignment_all",
-        "DLPFC_Br2720_ant_2",
-        "DLPFC_Br2743_post_manual_alignment",
-        "DLPFC_Br6471_ant_manual_alignment_all"
+      "DLPFC_Br2720_ant_2",
+      "DLPFC_Br2720_mid_manual_alignment",
+      "DLPFC_Br2720_post_extra_reads",
+      "DLPFC_Br2743_ant_manual_alignment",
+      "DLPFC_Br2743_mid_manual_alignment_extra_reads",
+      "DLPFC_Br2743_post_manual_alignment",
+      "DLPFC_Br3942_ant_manual_alignment",
+      "DLPFC_Br3942_mid_manual_alignment",
+      "DLPFC_Br3942_post_manual_alignment",
+      "DLPFC_Br6423_ant_manual_alignment_extra_reads",
+      "DLPFC_Br6423_mid_manual_alignment",
+      "DLPFC_Br6423_post_extra_reads",
+      "DLPFC_Br6432_ant_2",
+      "DLPFC_Br6432_mid_manual_alignment",
+      "DLPFC_Br6432_post_manual_alignment",
+      "DLPFC_Br6471_ant_manual_alignment_all",
+      "DLPFC_Br6471_mid_manual_alignment_all",
+      "DLPFC_Br6471_post_manual_alignment_all",
+      "DLPFC_Br6522_ant_manual_alignment_all",
+      "DLPFC_Br6522_mid_manual_alignment_all",
+      "DLPFC_Br6522_post_manual_alignment_all",
+      "DLPFC_Br8325_ant_manual_alignment_all",
+      "DLPFC_Br8325_mid_2",
+      "DLPFC_Br8325_post_manual_alignment_all",
+      "DLPFC_Br8492_ant_manual_alignment",
+      "DLPFC_Br8492_mid_manual_alignment_extra_reads",
+      "DLPFC_Br8492_post_manual_alignment",
+      "DLPFC_Br8667_ant_extra_reads",
+      "DLPFC_Br8667_mid_manual_alignment_all",
+      "DLPFC_Br8667_post_manual_alignment_all"
     ];
   };
 
