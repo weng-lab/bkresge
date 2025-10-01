816b89cc1fdac2cadb9af4b60e3993ef80bb8507
bgkresge <Ben.Kresge@umassmed.edu>
2025-10-01 11:44:30 -0400

    Paper data loader for data inspection

--- hippocampus/load_paper_data.R
+++ hippocampus/load_paper_data.R
@@ -3,8 +3,26 @@ library(SpatialExperiment)
 library(humanHippocampus2024)
 library(ExperimentHub)
 
-spe_output_path <- "/data/zusers/kresgeb/hippocampus/R_download/spatial_hpc_spe.Rdata"
-snrna_seq_output_path <- "/data/zusers/kresgeb/hippocampus/R_download/spatial_hpc_snrna_seq.Rdata"
+# Paths
+hpc_spe_path <- "/data/zusers/kresgeb/hippocampus/R_download/spatial_hpc_spe.Rdata"
+hpc_sce_path <- "/data/zusers/kresgeb/hippocampus/R_download/spatial_hpc_snrna_seq.Rdata"
+dlpfc_spe_path <- "/data/zusers/kresgeb/psych_encode/spatialLIBD_fetch_data/2024.RData"
+dlpfc_sce_path <- "/data/zusers/kresgeb/psych_encode/spatialDLPFC_snRNAseq_fetch/2024_snRNA.RData"
+
 message("Loading from files...")
-load(spe_output_path, verbose = TRUE)
-load(snrna_seq_output_path, verbose = TRUE)
+
+# Function to load and rename
+load_and_rename <- function(path, new_name) {
+  obj_names <- load(path, verbose = TRUE)
+  if (length(obj_names) != 1) {
+    stop(paste("Expected 1 object in", path, "but got", length(obj_names)))
+  }
+  assign(new_name, get(obj_names), envir = .GlobalEnv)
+  rm(list = obj_names, envir = .GlobalEnv)  # clean up original name
+}
+
+# Load each dataset with standardized names
+load_and_rename(hpc_spe_path, "hpc_spe")
+load_and_rename(hpc_sce_path, "hpc_sce")
+load_and_rename(dlpfc_spe_path, "dlpfc_spe")
+load_and_rename(dlpfc_sce_path, "dlpfc_sce")
